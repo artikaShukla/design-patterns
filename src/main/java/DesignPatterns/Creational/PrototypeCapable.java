474bb321b16b87e391123f51f25cc109d2195566
SG0225323 <Artika.Shukla@sabre.com>
2019-08-27 23:20:12 +0530

    Design Pattterns

---
+++ src/main/java/DesignPatterns/Creational/PrototypeCapable.java
@@ -0,0 +1,9 @@
+package DesignPatterns.Creational;
+
+public interface PrototypeCapable extends Cloneable {
+
+    public Object clone()throws CloneNotSupportedException;
+
+
+
+}
---
+++ src/main/java/DesignPatterns/Structural/AdapterDesignPattern.java
@@ -0,0 +1,7 @@
+package DesignPatterns.Structural;
+
+public class AdapterDesignPattern {
+}
+
+
+
